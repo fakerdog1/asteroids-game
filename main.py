7ea1f046f38c43c1e17d9f5a4c2fbbabe3cb10e8
Martin Simeonov <simeonoff.martin@gmail.com>
2025-08-04 10:50:02 +0300

    add 60 fps tickrate

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
 
         screen.fill("#000000")
         pygame.display.flip()
+        passed_time = clock.tick(60)
+        dt += passed_time/1000
 
 if __name__ == "__main__":
     main()
